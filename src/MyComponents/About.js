4f1e4626941031660562a1a03d7574fc9431549b
Stelvinsaji <87966154+Stelvinsaji@users.noreply.github.com>
2021-09-16 13:41:43 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            Decription
+            <p>A list of the tasks that you have to do, or things that you want to do:
+                Each day u can try to mark off as many items on my to-do list as possible.</p>
+        </div>
+    )
+}
